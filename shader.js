50d3eac2d64077adf7a6256509d61b89bdfedc8e
Mr Speaker <mrspeaker@gmail.com>
2017-08-06 16:10:15 -0400

    lil tweak

--- shader.js
+++ shader.js
@@ -1,9 +1,9 @@
 const vs = `#version 300 es
 
   in vec4 pos;
-  out vec4 p;
+  out vec4 screen;
   void main () {
-    p = pos;
+    screen = pos;
     gl_PointSize = 40.0;
     gl_Position = pos;
   }
@@ -13,11 +13,12 @@ const fs = `#version 300 es
   precision mediump float;
 
   out vec4 color;
-  in vec4 p;
+  in vec4 screen;
   void main () {
-    float dist = distance(vec2(0.5, 0.5), gl_PointCoord.xy);
+    vec2 uv = gl_PointCoord.xy;
+    float dist = distance(vec2(0.5, 0.5), uv);
     if (dist < 0.5) {
-      color = vec4(gl_PointCoord.x, abs(p.x*p.y), gl_PointCoord.y, 1.0);
+      color = vec4(uv.x, abs(screen.x * screen.y), uv.y, 1.0);
     } else {
       discard;
     }
